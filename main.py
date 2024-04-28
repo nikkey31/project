c09052da92ed08195438c44690ebdff9d2871206
nikkey31 <146629377+nikkey31@users.noreply.github.com>
2024-04-28 12:37:13 +0300

    'comment'

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.Text, primary_key=True, nullable=False)
+    password = db.Column(db.Text, primary_key=True, nullable=False)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
