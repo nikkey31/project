29290df334f80b777ce67f098bd3db33827c9fcc
nikkey31 <146629377+nikkey31@users.noreply.github.com>
2024-05-18 21:27:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ class User(db.Model):
 def index():
     return render_template('index.html')
 
+@app.route('/quiz')
+def quiz():
+    return render_template(
+                            'quiz.html', 
+                           )
 
 if __name__ == "__main__":
     app.run(debug=True)
